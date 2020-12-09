2a8886b393ed9d22a7057778b3ccdfc7714d93fe
maria-ribeiro <45239294+marybeiro@users.noreply.github.com>
2020-12-09 13:32:41 +0000

    Added link to github repo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Weather App
+     <h1>Weather App</h1>
+     <footer>
+       <a href="https://github.com/maria-ribeiro/react-weather-app" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+     </footer>
     </div>
   );
 }
