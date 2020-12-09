40763a4416f0d3e25d0c0a51250735d32c90385e
maria-ribeiro <45239294+marybeiro@users.noreply.github.com>
2020-12-09 21:16:40 +0000

    Added HTML and CSS for current weather: main info and description

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-       <div className="container">
+      <div className="container">
         <h1>Weather App</h1>
+        <Weather />
         <footer>
           <a href="https://github.com/maria-ribeiro/react-weather-app" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
         </footer>
---
+++ src/Weather.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Weather.css";
+
+function Weather(){
+  return(
+    <div className="Weather">
+      <form>
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" />
+      </form>
+      <div className="row align-items-center outter-row">
+        <div className="col-6 weather-main">
+          <i class="far fa-snowflake"></i>
+          <h2>
+            22°C
+          </h2>
+          <p>Snow</p>
+        </div>
+        <div className="col-6 weather-description">
+          <p>
+            Lisbon, PT <br/>
+            Wed, Dec 9 2020
+          </p>
+          <div className="row">
+            <div className="col-6">
+              <ul>
+                <li>Maximum 23</li>
+                <li>Humidity 63%</li>
+                <li>Sunrise 08:02</li>
+              </ul>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Minimum 15</li>
+                <li>Wind 4km/h</li>
+                <li>Sunset 18:21</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default Weather;
