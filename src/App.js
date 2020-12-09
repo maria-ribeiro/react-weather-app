aa8fc05a0588095db877ac35ec1f7b5d8030823b
maria-ribeiro <45239294+marybeiro@users.noreply.github.com>
2020-12-09 15:04:05 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Weather App</h1>
-     <footer>
-       <a href="https://github.com/maria-ribeiro/react-weather-app" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
-     </footer>
+       <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a href="https://github.com/maria-ribeiro/react-weather-app" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+        </footer>
+      </div>
     </div>
   );
 }
